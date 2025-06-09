22220cacc4c8e4192aa9f5416ec4e680c131ffe9
Shreyas-Nagabhushan <110516607+Shreyas-Nagabhushan@users.noreply.github.com>
2025-06-09 06:37:02 +0530

    Update

--- Express/DbHandler.js
+++ Express/DbHandler.js
@@ -5,12 +5,12 @@ class DbHandler
     constructor(url)
     {
         this.url = url; 
-        this.client = null;
+        this.client = new MongoClient(url);
     }
 
     async connect()
     {
-        this.client = await MongoClient.connect(this.url);
+        this.client = await this.client.connect(this.url);
         console.log("Connected to DB");
     }
 
